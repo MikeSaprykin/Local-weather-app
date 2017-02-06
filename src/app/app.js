e9c6f9da9f82cbed88404980c82fed4a5bbb3d6d
mikesaprykin <mikesaprykin@gmail.com>
2017-02-06 12:22:26 +0200

    updated config, dist and docs index

--- src/app/app.js
+++ src/app/app.js
@@ -17,38 +17,43 @@ var constants = require('./constants');
     $.ajax({
         url: constants.IP_API,
         dataType:"jsonp",
-        success: function(results){
-            var currentLat = "lat=" + results.lat;
-            var currentLon = "lon=" + results.lon;
-            requestMyWeather(currentLat,currentLon);
+        success: function(response){
+            onIpRequestSuccess(response)
         }
     });
+
+    function onIpRequestSuccess(results){
+         var currentLat = "lat=" + results.lat;
+         var currentLon = "lon=" + results.lon;
+         requestMyWeather(currentLat,currentLon);
+         $('div.location p span').html(results.city + ", " + results.country);
+     }
 
     function requestMyWeather(lat,lon){
         $.ajax({
             url: constants.OPEN_WEATHER_API + lat + "&" + lon + "&units=metric" + constants.OPEN_WEATHER_APP_ID,
             dataType: 'jsonp',
-            success: function(results){
-                fillAppLayout(results)
+            success: function(response){
+                fillAppLayout(response)
             }
         });
     }
 
     function fillAppLayout(results){
         var windSpeed = Math.round(results.wind.speed);
-        var hummidity = results.main.humidity;
+        var humidity = results.main.humidity;
         var temperature = Math.round(results.main.temp);
-        var weatherConditions = results.weather[0].main;
+        var weatherConditions = results.weather[0].description;
         var temperatureFahr = Math.round(temperature * 9/5 +32);
 
-        $('div.location p span').html(results.city + ", " + results.country);
-        $('div.celsium p span').html(temperature + " ");
-        $('div.farenheit p span').html(temperatureFahr + " ");
-        $('div.humidity p span').html(hummidity + " ");
-        $('div.wind p span').html(windSpeed + " ");
-        $('div.description img.icon').attr("src", constants.CONDITIONS_CONSTS[weatherConditions].image);
-        $('.weather-app-bg').css("background-color", constants.CONDITIONS_CONSTS[weatherConditions].background);
+        $('div.celsium span').html(temperature + " ");
+        $('div.farenheit span').html(temperatureFahr + " ");
+        $('div.humidity span').html(humidity + " ");
+        $('div.wind span').html(windSpeed + " ");
+        $('div.description').html(results.weather[0].main);
+        $('img.icon').attr("src", constants.CONDITIONS_CONSTS[weatherConditions].image);
+        $('.weather-app-bg').css("background-color", constants.CONDITIONS_CONSTS[weatherConditions].background)
+            .fadeIn('fast');
     }
 
-     $(".weather-app-bg").fadeIn("fast");
 });
